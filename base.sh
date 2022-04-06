23bdea6249fc5cfff0cd2b489b96f5603c55b93f
Johannes <103150154+justjohannes@users.noreply.github.com>
2022-04-07 01:05:41 +0200

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
+hwclock --systohc
+sed -i '178s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=us-acentos" >> /etc/vconsole.conf
+echo "archlinux" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
+echo root:PASSWORD | chpasswd
+
+
+pacman -S refind efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font nvidia nvidia-utils nvidia-settings
+
+
+systemctl enable NetworkManager
+
+useradd -m johannes
+echo johannes:PASSWORD | chpasswd
+usermod -aG wheel,audio,video johannes
+
+echo "johannes ALL=(ALL) ALL" >> /etc/sudoers.d/johannes
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
