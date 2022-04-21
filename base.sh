09ed7c4b8aae86756881bf4d3eae6b227f0885dd
Johannes <103150154+justjohannes@users.noreply.github.com>
2022-04-22 01:04:43 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -6,12 +6,11 @@ sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us-acentos" >> /etc/vconsole.conf
-echo "archlinux" >> /etc/hostname
+echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
-echo root:PASSWORD | chpasswd
-
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
 
 pacman -S refind efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font nvidia nvidia-utils nvidia-settings
 
